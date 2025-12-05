ef5f3c090e2e64b62e72b6335f82cb3e2c043131
Adam Cmiel <acmiel@redhat.com>
2025-12-05 17:59:12 +0100

    Day 03: part 2
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- 03/solve.hs
+++ 03/solve.hs
@@ -11,15 +11,20 @@ parseInput = map (map digitToInt) . lines
 leftMostMaxBy :: (a -> a -> Ordering) -> [a] -> a
 leftMostMaxBy f = minimumBy (flip f)
 
-joltage :: [Int] -> Int
-joltage bank =
-  let m = leftMostMaxBy (compare `on` head) $ filter ((>= 2) . length) (tails bank)
-   in 10 * head m + maximum (tail m)
+joltage :: Int -> [Int] -> Int
+joltage 1 bank = maximum bank
+joltage n bank =
+  let m = leftMostMaxBy (compare `on` head) $ filter ((>= n) . length) (tails bank)
+   in (10 ^ (n - 1)) * head m + joltage (n - 1) (tail m)
 
 part1 :: [[Int]] -> Int
-part1 = sum . map joltage
+part1 = sum . map (joltage 2)
+
+part2 :: [[Int]] -> Int
+part2 = sum . map (joltage 12)
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
   print $ part1 input
+  print $ part2 input
