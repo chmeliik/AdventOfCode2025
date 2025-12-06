cdba46d11b7921e49a038f081d69a1498015ec6d
Adam Cmiel <acmiel@redhat.com>
2025-12-07 00:42:15 +0100

    Day 06: part 2
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- 06/solve.hs
+++ 06/solve.hs
@@ -1,23 +1,42 @@
 module Main (main) where
 
+import Data.Bifunctor (Bifunctor (second))
+import Data.Char (isSpace)
 import Data.List (transpose)
 
 data Op = Add | Mul
 
-parseInput :: String -> [(Op, [Int])]
-parseInput = map (parse . reverse) . transpose . map words . lines
+parseInput :: String -> [(Op, [String])]
+parseInput = map parseColumn . columns
   where
-    parse :: [String] -> (Op, [Int])
-    parse (op : nums) = (if op == "+" then Add else Mul, map read nums)
+    columns = splitBy (all isSpace) . transpose . lines
+
+    parseColumn :: [String] -> (Op, [String])
+    parseColumn c = (parseOp (last (transpose c)), init (transpose c))
+
+    parseOp :: String -> Op
+    parseOp s
+      | '+' `elem` s = Add
+      | otherwise = Mul
+
+    splitBy :: (a -> Bool) -> [a] -> [[a]]
+    splitBy p xs =
+      case break p xs of
+        (h, []) -> [h]
+        (h, _ : t) -> h : splitBy p t
 
 eval :: Op -> [Int] -> Int
 eval Add = sum
 eval Mul = product
 
-part1 :: [(Op, [Int])] -> Int
-part1 = sum . map (uncurry eval)
+part1 :: [(Op, [String])] -> Int
+part1 = sum . map (\(op, nums) -> eval op (map read nums))
+
+part2 :: [(Op, [String])] -> Int
+part2 = part1 . map (transpose `second`)
 
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
   print $ part1 input
+  print $ part2 input
